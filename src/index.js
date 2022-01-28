0cb6684096a4e1e52fb1dab50e2aa90b8687e470
Daskendrik <ss599@yandex.ru>
2022-01-28 19:19:47 +0300

    Square become func

--- src/index.js
+++ src/index.js
@@ -2,20 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-
-  
-    render() {
-      return (
-        <button 
+function Square(props) {
+  return(
+    <button 
           className="square" 
-          onClick={() => this.props.onClick()}
-        >
-          {this.props.value}
-        </button>
-      );
-    }
-  }
+          onClick={() => props.onClick()}>
+          {props.value}
+    </button>
+  );
+}
+
+
   
   class Board extends React.Component {
     constructor(props) {
